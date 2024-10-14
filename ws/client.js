58de508ee9d6cef5a3d25a8172983356c8bea218
okunbro <okunbro@gmail.com>
2024-10-14 14:39:06 +0300

    task 5

---
+++ ws/client.js
@@ -0,0 +1,17 @@
+const {WebSocket}=require('ws');
+
+const wsClientFactory=(id)=>{
+  const ws = new WebSocket('ws://localhost:8080');
+  ws.on('error', console.error);
+  ws.on('open', function open(){
+    console.log(`connected ${id}`);
+    ws.send(`${id} hello`);
+  });
+  ws.on('message', function message(data){
+    console.log(` ${id} received: %s`, data);
+   
+  });
+}
+const wsClient1 =wsClientFactory(1);
+const wsClient2 =wsClientFactory(2);
+const wsClient3 =wsClientFactory(3);
